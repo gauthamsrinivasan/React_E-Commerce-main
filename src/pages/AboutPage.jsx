14718661789d1a9f248f975d9ccde38dc1193e06
gauthamsrinivasan <gauthamsrinivasan2021@gmail.com>
2023-11-25 14:58:57 +0530

    aboutus

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,19 +8,7 @@ const AboutPage = () => {
         <h1 className="text-center">About Us</h1>
         <hr />
         <p className="lead text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          facere doloremque veritatis odit similique sequi. Odit amet fuga nam
-          quam quasi facilis sed doloremque saepe sint perspiciatis explicabo
-          totam vero quas provident ipsam, veritatis nostrum velit quos
-          recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo
-          earum unde eligendi autem praesentium, doloremque distinctio nesciunt
-          porro tempore quis eaque labore voluptatibus ea necessitatibus
-          exercitationem tempora molestias. Ad consequuntur veniam sequi ullam
-          tempore vel tenetur soluta dolore sunt maxime aliquam corporis est,
-          quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit
-          delectus expedita a alias nam recusandae illo debitis repellat libero,
-          quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam
-          dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+        Welcome to our E-commerce venture! Crafted by a dynamic trio of students enrolled in the Naan Mudhalvan course, this platform is a testament to our shared vision for innovative solutions in the digital realm. Our diverse skills converge to bring you an immersive online shopping experience that goes beyond conventional boundaries. From user-friendly interfaces to cutting-edge features, we've harnessed the knowledge gained in the Naan Mudhalvan course to create a platform that seamlessly blends functionality and aesthetics. Join us on this exciting journey as we redefine the online shopping landscape, one click at a time.
         </p>
 
         <h2 className="text-center py-4">Our Products</h2>
